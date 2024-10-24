f81bfce0f76fb19753a4ccfaef5e0fdfda001969
a-b-h-a-y-s-h-i-n-d-e <theabhay.shinde@gmail.com>
2024-10-24 19:18:14 +0530

    SSL certificate added for API

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
   const handleSubmit = async (event) => {
     event.preventDefault()
     
-    const apiURL = 'http://13.51.64.135:8000/generate/'
+    const apiURL = 'https://13.51.64.135:8000/generate/'
 
     try{
       const response = await fetch(apiURL, {
